0290c2d4305b88291b3d95e92fbaee127e183560
tempdeltavalue <mykhailo.mel1122@gmail.com>
2024-12-12 18:52:17 +0200

    Init node.js mongodb, user auth

---
+++ chat_app/server/Models/userModel.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose")
+
+const userSchema = new mongoose.Schema(
+    {
+        name:{type: String, required: true, minlength:3, maxlength:30}, 
+        email:{type: String, required: true, minlength:3, maxlength:30, unique: true}, 
+        password:{type: String, required: true, minlength:3, unique: true}, 
+    }, {
+        timestamps:true,
+    }
+);
+
+const userModel = mongoose.model("User", userSchema);
+
+module.exports = userModel;
